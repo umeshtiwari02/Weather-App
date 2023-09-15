5443184219d839f5dde11a2fe45c05ec5886d9d5
Umesh Tiwari <klau.tiwari1234@gmail.com>
2023-09-15 12:48:32 +0530

    shows-current-weather-condition

---
+++ lib/secrets.dart
@@ -0,0 +1,10 @@
+const openWeatherAPIKey = '2dc98b8a8e698739796377504f5a5c48';
+
+// 28.67571563320335, 80.95878962628258
+
+
+// api.openweathermap.org/data/2.5/forecast?lat=28.67571563320335&lon=80.95878962628258&appid=2dc98b8a8e698739796377504f5a5c48
+
+
+
+// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2dc98b8a8e698739796377504f5a5c48
